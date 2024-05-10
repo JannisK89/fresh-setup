074be5b194c4652b1d24de63de3c307408a3ee56
JannisK89 <jannis.karanikis@gmail.com>
2024-05-10 20:19:41 +0200

    added missing steps

--- main.sh
+++ main.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
 # Update package list
-echo "Updating package list..."
+echo "Updating package list"
 sudo apt update
 
 # Install Zsh
-echo "Installing Zsh..."
+echo "Installing Zsh"
 sudo apt install zsh -y
-
-# Set Zsh as the default shell
-echo "Setting Zsh as the default shell..."
 chsh -s $(which zsh)
 
 # Install Oh-My-Zsh
-echo "Installing Oh-My-Zsh..."
+echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Clone the Powerlevel10k theme into the Oh-My-Zsh custom themes directory
-echo "Installing Powerlevel10k theme..."
+echo "Installing Powerlevel10k theme, plugins and config file"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Set the theme in .zshrc
-echo "Configuring .zshrc for Powerlevel10k..."
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+$ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+$ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+wget https://github.com/JannisK89/config-files/blob/main/.tmux.conf
 
 # Install tmux
-echo "Installing tmux..."
+echo "Installing tmux, tpm and config file."
 sudo apt install tmux -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+wget https://raw.githubusercontent.com/JannisK89/config-files/main/.tmux.conf
 
 # Install and setup Neovim
-echo "Installing Neovim..."
+echo "Installing Neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
+sudo apt install libfuse2
+sudo apt install gcc
 
 # Clone Neovim configuration
-echo "Setting up Neovim configuration..."
+echo "Setting up Neovim configuration"
 mkdir -p ${HOME}/.config/nvim
 git clone https://github.com/JannisK89/init.lua ${HOME}/.config/nvim
-
-# Install recommended font for Powerlevel10k and Neovim
-echo "Installing the recommended Meslo Nerd Font..."
-FONT_PATH="$HOME/.local/share/fonts"
-mkdir -p $FONT_PATH
-curl -fLo "$FONT_PATH/MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-curl -fLo "$FONT_PATH/MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-curl -fLo "$FONT_PATH/MesloLGS NF Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-curl -fLo "$FONT_PATH/MesloLGS NF Bold Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 # Source the .zshrc to apply changes
 echo "Sourcing .zshrc..."
