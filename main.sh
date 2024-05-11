dd03dd00e9b6fde1631bff6f1e33028331f7878b
JannisK89 <jannis.karanikis@gmail.com>
2024-05-11 07:38:22 +0200

    removed $ lol

--- main.sh
+++ main.sh
@@ -37,8 +37,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Clone the Powerlevel10k theme into the Oh-My-Zsh custom themes directory
 echo "Installing Powerlevel10k theme, plugins and config file"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-$ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-$ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 rm .zshrc
 wget https://raw.githubusercontent.com/JannisK89/config-files/main/.zshrc
 
